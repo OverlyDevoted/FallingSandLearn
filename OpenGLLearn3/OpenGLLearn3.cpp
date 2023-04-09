8fcf6067e0466462e99d87acdcb208e9d862339b
OverlyDevoted <58229165+OverlyDevoted@users.noreply.github.com>
2023-04-09 23:50:52 +0300

    Camera, ImGui  - Model, view, projection implementation  - Contructing 3D cube  - ImGui camera control

--- OpenGLLearn3/OpenGLLearn3.cpp
+++ OpenGLLearn3/OpenGLLearn3.cpp
@@ -1,7 +1,9 @@
+#include "src/vendor/imgui/imgui.h"
+#include "src/vendor/imgui/imgui_impl_glfw.h"
+#include "src/vendor/imgui/imgui_impl_opengl3.h"
 // OpenGLLearn3.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 #include <glad/glad.h>
-
 //GLFW is a library, written in C, specifically targeted at OpenGL. GLFW gives us the bare necessities required for rendering goodies to the screen. It allows us to create an OpenGL context, define window parameters, and handle user input, which is plenty enough for our purposes. 
 #include <GLFW/glfw3.h>
 
@@ -9,6 +11,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
 
 //image lib
 #define STB_IMAGE_IMPLEMENTATION
@@ -24,7 +27,7 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 unsigned int VAO[2], VBO[2], EBO[2];
-float mix = 0.5f;
+float* mix = new float(0.5f);
 void createTriangle(unsigned int& VAO, unsigned int& VBO, unsigned int& EBO, float* vertices)
 {
     int indices[] = { //briaunos. there was a note to start from one
@@ -62,6 +65,83 @@ void createTriangle(unsigned int& VAO, unsigned int& VBO, unsigned int& EBO, flo
     //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
+
+void createCube(unsigned int& VAO, unsigned int& VBO)
+{
+    float vertices[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    //assigns an ID to a vbo
+    glGenBuffers(1, &VBO);
+    //we bind the buffer so any buffer calls we make on the target GL_ARRAY_BUFFER will be used to modify the currently bound buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // so as you can see we specify a buffer GL_ARRAY_BUFFER, this tells opengl that we are configuring that buffer and then with this command we send the vertex data to the gpu
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 180, vertices, GL_STATIC_DRAW);
+
+    //element buffer which says how to render triangles
+    //glGenBuffers(1, &EBO);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    //linking vertex attributes is basically saying how to vertex data is stored, what goes into a single vertex
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    //glEnableVertexAttribArray(1);
+
+    // texture attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
 const int WIDTH = 800, HEIGTH = 600;
 int main()
 {
@@ -85,11 +165,15 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    glEnable(GL_DEPTH_TEST);
     //setting the viewport dimensions. in this case opengl will take the whole viewport if we made it to look smaller the normalized device coordinates would be transformed to fit into 
     //specified witdh and heigth
     glViewport(0, 0, WIDTH, HEIGTH);
     //registering resize callback 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
     //triangle vertices
     float vertices[] = {
         // positions         // colors           //texture coords
@@ -102,7 +186,7 @@ int main()
     Texture texture2 = Texture("src/res/textures/Okay.png", GL_RGBA, true);
     //vao and vbo for a triangle setup
     createTriangle(VAO[0], VBO[0], EBO[0], vertices);
-    createTriangle(VAO[1], VBO[1], EBO[1], vertices);
+    createCube(VAO[1], VBO[1]);
 
 
     //shader program object setup
@@ -112,16 +196,44 @@ int main()
     glUniform1i(glGetUniformLocation(shader.ID, "texture1"), 0);
     glUniform1i(glGetUniformLocation(shader.ID, "texture2"), 1);
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-    glm::mat4 view = glm::mat4(1.0f);
-    // note that we're translating the scene in the reverse direction of where we want to move
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
-    glm::mat4 projection;
-    projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    //projection = glm::rotate(projection, glm::radians(15.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f,  0.0f,  0.0f),
+    glm::vec3(2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
 
+
+
+    //IMGUI setup
+    // Setup Dear ImGui context
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+
+    ImGui_ImplOpenGL3_Init("#version 330");
+    bool show_demo_window = true;
+    bool show_another_window = true;
+    float position[] = { 0.0f,0.0f,3.0f };
     //render loop, keeps the program open until we tell it to close
     while (!glfwWindowShouldClose(window))
     {
@@ -135,7 +247,14 @@ int main()
 
         //draw
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+
 
         //set uniform for the first shader
         //get uniform location in the shader program getting the location does not require for the program to be used 
@@ -145,46 +264,73 @@ int main()
         //first triangle
         shader.use();
         //set the value based on location. To set the value the program has to be in use hence glUseProgram is called first
-        glm::mat4 trans = glm::mat4(1.0f);
 
-        trans = glm::translate(trans, glm::vec3(0.5, -0.5, 0.0));
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0, 0.0, 1.0));
 
-        shader.setUniform4m("trans", trans);
-        shader.setUniform1f("mixF", mix);
-
-        glBindVertexArray(VAO[0]);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        
-        glm::mat4 secondTrans = glm::mat4(1.0f);
-        secondTrans = glm::translate(secondTrans, glm::vec3(-0.5, 0.5, 0.0));
-        float scaleAmount = (sinf(glfwGetTime()) + 1) / 2;
-        secondTrans = glm::scale(secondTrans, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
-
-        std::cout << "Time: " << glfwGetTime() << " SIN: " << sinf(glfwGetTime()) << " Clamped: " << (sinf(glfwGetTime()) + 1) / 2 << std::endl;
-        shader.setUniform4m("trans", secondTrans);
 
         glBindVertexArray(VAO[1]);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        
-        //second triangle
-        //glUseProgram(shaderProgram2);
-        //glBindVertexArray(VAO[1]);
-        //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+        glm::mat4 view = glm::mat4(1.0f);
+        // note that we're translating the scene in the reverse direction of where we want to move
+        view = glm::translate(view, glm::vec3(-position[0], -position[1], -position[2]));
+
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(50.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+        shader.setUniform4m("projection", projection);
+        shader.setUniform4m("view", view);
+        for (int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, glm::vec3(cubePositions[i]));
+            if (i % 3 == 0 || i == 0)
+            {
+                model = glm::rotate(model, (float)glfwGetTime() * glm::radians(-55.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+            }
+            shader.setUniform4m("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        shader.setUniform1f("mixF", *mix);
+
+        // glBindVertexArray(VAO[0]);
+         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
+         //second triangle
+         //glUseProgram(shaderProgram2);
+         //glBindVertexArray(VAO[1]);
+         //glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
+
+
+
+        ImGui::Begin("My window");
+        ImGui::Text("Hello ImGUI");
+        ImGui::SliderFloat3("Camera pos", position, -3, 3);
+        ImGui::SliderFloat("Okay visibility", mix, 0, 1);
+        ImGui::End();
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 
         //front buffer contains current screen image, while all other draw functions are performed onto the second buffer which is then swapped with this command after drawing all the geometries
         //this prevents user from seeing artifacts from drawn geometries at runtime
         glfwSwapBuffers(window);
 
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         //checks if any event have been triggered (i/o inputs)
         glfwPollEvents();
     }
-    //good practice is to delete your vao, vbo and shader programs
 
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    //good practice is to delete your vao, vbo and shader programs
     glDeleteBuffers(2, VAO);
     glDeleteBuffers(2, VBO);
 
+
+    glfwDestroyWindow(window);
     //terminate glfw session
     glfwTerminate();
     return 0;
@@ -203,15 +349,15 @@ void processInput(GLFWwindow* window)
         printf("W key pressed");
     else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
     {
-        if (mix <= 1.0f)
-            mix += 0.0025;
-        printf("%f", mix);
+        if (*mix <= 1.0f)
+            *mix += 0.0025;
+        printf("%f", *mix);
     }
     else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
     {
-        if (mix > 0.0f)
-            mix -= 0.0025;
-        printf("%f", mix);
+        if (*mix > 0.0f)
+            *mix -= 0.0025;
+        printf("%f", *mix);
     }
 }
 
--- OpenGLLearn3/src/res/shaders/vertexShader.glsl
+++ OpenGLLearn3/src/res/shaders/vertexShader.glsl
@@ -1,17 +1,18 @@
 #version 330 core
-layout(location = 0) in vec3 aPos; 
+layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aCol;
 layout(location = 2) in vec2 aTexCoord;
 
 out vec3 vertexColor;
 out vec2 TexCoord;
 
-uniform mat4 trans;
-
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-    gl_Position = trans * vec4(aPos.xyz, 1.0);
+    gl_Position = projection * view * model * vec4(aPos.xyz, 1.0);
     vertexColor = aCol;
     TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
